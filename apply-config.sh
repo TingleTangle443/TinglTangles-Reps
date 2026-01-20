e180cfc3cf2f4b45bc5ec832bd13a920db5d0996
TingleTangle443 <tbischofsberger@yahoo.de>
2026-01-20 22:04:11 +0100

    Update apply-config.sh

--- apply-config.sh
+++ apply-config.sh
@@ -1,32 +1,24 @@
-#!/usr/bin/with-contenv bash
+#!/bin/bash
 set -e
 
 CONFIG_FILE="/etc/shairport-sync.conf"
 
-echo "[INFO] Recreating shairport-sync.conf (ALSA only)"
+echo "[INFO] Generating shairport-sync.conf"
 
-# Alte Konfig kompromisslos entfernen
-rm -f /etc/shairport-sync.conf
-rm -f /etc/shairport-sync.conf.sample
-
-# Werte aus Home Assistant
 AIRPLAY_NAME="${airplay_name:-Home Assistant AirPlay}"
-ALSA_DEVICE="${alsa_device:-hw:1,0}"
-
+ALSA_DEVICE="${alsa_device:-hw:0,0}"
 MQTT_HOST="${mqtt_host:-}"
 MQTT_PORT="${mqtt_port:-1883}"
 MQTT_USERNAME="${mqtt_username:-}"
 MQTT_PASSWORD="${mqtt_password:-}"
 MQTT_TOPIC="${mqtt_topic:-shairport-sync}"
 
-# Neue Config schreiben (ALSA ONLY)
 cat > "$CONFIG_FILE" <<EOF
 general =
 {
   name = "${AIRPLAY_NAME}";
+  output_backend = "alsa";
 };
-
-output_backend = "alsa";
 
 alsa =
 {
@@ -35,31 +27,24 @@ alsa =
 };
 EOF
 
-# MQTT optional
 if [ -n "$MQTT_HOST" ]; then
 cat >> "$CONFIG_FILE" <<EOF
 
 mqtt =
 {
+  enabled = "yes";
   hostname = "${MQTT_HOST}";
   port = ${MQTT_PORT};
   topic = "${MQTT_TOPIC}";
 EOF
 
-  if [ -n "$MQTT_USERNAME" ]; then
-    echo "  username = \"${MQTT_USERNAME}\";" >> "$CONFIG_FILE"
-  fi
-
-  if [ -n "$MQTT_PASSWORD" ]; then
-    echo "  password = \"${MQTT_PASSWORD}\";" >> "$CONFIG_FILE"
-  fi
+[ -n "$MQTT_USERNAME" ] && echo "  username = \"${MQTT_USERNAME}\";" >> "$CONFIG_FILE"
+[ -n "$MQTT_PASSWORD" ] && echo "  password = \"${MQTT_PASSWORD}\";" >> "$CONFIG_FILE"
 
 cat >> "$CONFIG_FILE" <<EOF
 };
 EOF
 fi
 
-echo "[INFO] Final shairport-sync.conf:"
-echo "---------------------------------"
+echo "[INFO] shairport-sync.conf created:"
 cat "$CONFIG_FILE"
-echo "---------------------------------"
